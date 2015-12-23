2fff840268fb14a6e4be4cd4109394d4e9c290d9
Mark Tchao <mtchao@uw.edu>
2015-12-22 22:33:32 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+angular.module('CollectiveApp', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: 'views/home.html'
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: 'views/about.html'
+            });
+        $urlRouterProvider.otherwise('/home');
+    });
